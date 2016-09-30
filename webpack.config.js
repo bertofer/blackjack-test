825092cb3da38928d9873b079989bd5cf81acc14
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2016-09-30 08:47:45 +0100

    Skeleton webpack with angular

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devtool: 'source-map',
+  context: __dirname + '/client',
+  entry: './src/index.js',
+  output: {
+    path: './client/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: [/node_modules/], loader: 'ng-annotate!babel' },
+      { test: /\.html$/, loader: 'raw' },
+      { test: /\.sass$/, loader: 'style!css!sass' },
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  },
+  plugins: [
+    // Injects bundles in your index.html instead of wiring all manually.
+    // It also adds hash to all injected assets so we don't have problems
+    // with cache purging during deployment.
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+      hash: true
+    }),
+
+    // Automatically move all modules defined outside of application directory to vendor bundle.
+    // If you are using more complicated project structure, consider to specify common chunks manually.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module, count) {
+        return module.resource && module.resource.indexOf(path.resolve(__dirname, 'client')) === -1
+      }
+    })
+  ]
+}
